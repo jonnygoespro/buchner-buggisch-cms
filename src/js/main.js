b4c6eac22a26dff0fa30b5f2b70a350da9525ecc
Alexander Franz Josef Buchner <abuchner.mmt-b2021@fh-salzburg.ac.at>
2022-12-28 13:03:59 +0100

    run lint

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,36 @@
 import './../scss/style.scss'
 
 // Nav background color
-const navbar = document.querySelector("header")
+const navbar = document.querySelector('header')
 
-document.addEventListener("scroll", () => {
-    // console.log(window.scrollY)
-    if (window.scrollY > 100) {
-        navbar.classList.add("active")
-    } else {
-        navbar.classList.remove("active")
-
-    }
+document.addEventListener('scroll', () => {
+  // console.log(window.scrollY)
+  if (window.scrollY > 100) {
+    navbar.classList.add('active')
+  } else {
+    navbar.classList.remove('active')
+  }
 })
 
-
 // Burger menu
-const burger = document.querySelector("#burger")
-const header = document.querySelector("header")
-const links = document.querySelectorAll("header .mainnav a")
-let open = false;
+const burger = document.querySelector('#burger')
+const header = document.querySelector('header')
+const links = document.querySelectorAll('header .mainnav a')
+let open = false
 
-burger.addEventListener("click", () => {
-    open = !open;
+burger.addEventListener('click', () => {
+  open = !open
 
-    if (open) {
-        header.classList.add("open")
-    } else {
-        header.classList.remove("open")
-    }
+  if (open) {
+    header.classList.add('open')
+  } else {
+    header.classList.remove('open')
+  }
 })
 
 links.forEach((link) => {
-    link.addEventListener("click", () => {
-        open = false;
-        header.classList.remove("open")
-    })
+  link.addEventListener('click', () => {
+    open = false
+    header.classList.remove('open')
+  })
 })
