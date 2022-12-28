a35e446ebff4c06a3f7522cb3c899b05528c4350
Alexander Franz Josef Buchner <abuchner.mmt-b2021@fh-salzburg.ac.at>
2022-12-28 16:22:45 +0100

    added favicons

--- public/wp-content/themes/benbaeckt/index.php
+++ public/wp-content/themes/benbaeckt/index.php
@@ -6,6 +6,15 @@
     <title>CMS Endabgabe</title>
     <script type="module" crossorigin src='<?php echo get_template_directory_uri();?>/main.js' defer></script>
     <link rel='stylesheet' href='<?php echo get_template_directory_uri();?>/style.css'>
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri();?>/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();?>/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri();?>/favicons/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_template_directory_uri();?>/favicons/site.webmanifest">
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri();?>/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="<?php echo get_template_directory_uri();?>/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
     <?php wp_head(); ?>
   </head>
   <body>
