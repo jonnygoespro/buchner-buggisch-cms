b9c849e60ca04ade7cef1fbc0ee558a53061a44f
Jonas Buggisch <jonas.buggisch@gmail.com>
2022-12-28 12:48:25 +0100

    Small styling things adjusted

--- src/js/main.js
+++ src/js/main.js
@@ -17,7 +17,7 @@ document.addEventListener("scroll", () => {
 // Burger menu
 const burger = document.querySelector("#burger")
 const header = document.querySelector("header")
-const links = document.querySelectorAll("header .mainnav  a")
+const links = document.querySelectorAll("header .mainnav a")
 let open = false;
 
 burger.addEventListener("click", () => {
