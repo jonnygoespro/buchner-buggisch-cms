62f0a231e2f2579503bd36773ffb92199cfb0c3a
Alexander Franz Josef Buchner <abuchner.mmt-b2021@fh-salzburg.ac.at>
2023-01-03 14:23:22 +0100

    adjusted workshop imgs

--- public/wp-content/themes/benbaeckt/index.php
+++ public/wp-content/themes/benbaeckt/index.php
@@ -56,13 +56,14 @@
           ?>
             <p><?php the_title(); ?></p>
             <?php the_content(); ?>
-          <?php
+        </div>
+        <?php the_post_thumbnail(); ?>
+        <?php
 
               endwhile;
             endif;
             wp_reset_postdata();
           ?>
-        </div>
       </section>
 
       <section id="motto">
@@ -114,6 +115,7 @@
                   <path d="M49.0607 13.0607C49.6464 12.4749 49.6464 11.5251 49.0607 10.9393L39.5147 1.3934C38.9289 0.807612 37.9792 0.807612 37.3934 1.3934C36.8076 1.97918 36.8076 2.92893 37.3934 3.51472L45.8787 12L37.3934 20.4853C36.8076 21.0711 36.8076 22.0208 37.3934 22.6066C37.9792 23.1924 38.9289 23.1924 39.5147 22.6066L49.0607 13.0607ZM0 13.5H48V10.5H0V13.5Z" fill="black"/>
                 </svg>           </div>
                 <?php the_content(); ?>
+                <?php the_post_thumbnail(); ?>
               </button>
             <?php
                 endwhile;
